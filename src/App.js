a9e09aac0154743da43832d307af3545f530fbb8
darthcayonauta <cguzmanherr@gmail.com>
2024-05-04 19:20:56 -0400

    añadiendo carro de compras

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Component } from 'react';
 import Productos from './componets/Productos';
 import Layout from './componets/Layout';
+import Navbar from './componets/Navbar';
+
+import Title from './componets/Title';
 
 class App extends Component{
  state = {
@@ -17,18 +20,37 @@ class App extends Component{
       name:'Lechuga',
       price : 500,
       img:'/productos/lechuga.jpg' }
+  ],
+  carro: [
+   // {
+   //   name:'Tomate',
+   //   price : 1500,
+   //   img:'/productos/tomate.jpg', cantidad:1},
   ]
  }
  
+ agregarAlCarro = (producto)=>{
+  //console.log(producto);
+  return this.setState({
+    //añadimos elementos al arreglo vacío definido antes
+    carro: this.state.carro.concat({
+      ...producto, ///una copia del produco + cantidad
+      cantidad:1
+    })
+  })
+ }
  
   render(){
+    console.log(this.state.carro);
     return(
      <div> 
+      <Navbar />
       <Layout>
-      <Productos 
-        agregarAlCarro = {()=> console.log('soon')}
-        productos = {this.state.productos}
-      />
+        <Title />
+        <Productos 
+          agregarAlCarro = {()=> this.agregarAlCarro()}
+          productos = {this.state.productos}
+        />
       </Layout>
     </div>
     )
